02b4f51827a46b0b8e57c0ad7165fac5e7c73f57
zhangchunjie <zhangchunjie@easyway.net.cn>
2026-03-02 16:32:05 +0800

    1212121

--- test-homework.py
+++ test-homework.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.actions import interaction
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.pointer_input import PointerInput
-
+print(11)
 options = AppiumOptions()
 options.load_capabilities({
 	"platformName": "Android",
